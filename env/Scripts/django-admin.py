77f3fea55fd989518d14309041a79515bf93744a
Umair Afzal <mumairafzal2010@gmail.com>
2019-10-26 00:24:33 -0400

    Removed env folder, added Requirements.txt

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\mumai\OneDrive\Documents\0DATA\laptop data\ALL DATA\Umair DATA\NEW\personal work stuff\MERMER-frontend\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
